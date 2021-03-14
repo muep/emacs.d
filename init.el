47147244cceecad16dd28c58e8e68778279da412
Joonas Sarajärvi <muep@iki.fi>
2021-03-14 17:30:29 +0200

    Add some keyboard shortcuts for org mode

--- init.el
+++ init.el
@@ -34,6 +34,10 @@
 (global-set-key (kbd "C-x C-b") 'buffer-menu)
 (global-set-key (kbd "C-c d") 'delete-trailing-whitespace)
 (global-set-key (kbd "C-c f") 'describe-face)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-switchb)
 (if (functionp 'magit-status)
     (global-set-key (kbd "C-x g") 'magit-status))
 
