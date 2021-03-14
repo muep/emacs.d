e02d4495c9f7308515fdf0c13663914f0cad1653
Joonas Sarajärvi <muep@iki.fi>
2021-03-14 17:47:59 +0200

    Add eval-and-replace function

--- init.el
+++ init.el
@@ -391,3 +391,15 @@
   ; good for electrobibliomancy.
   (interactive)
   (goto-line (1+ (random (buffer-line-count)))))
+
+;; From https://emacsredux.com/blog/2013/06/21/eval-and-replace/
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+(global-set-key (kbd "C-c e") 'eval-and-replace)
