4b27db2293affd80a2bb0a04434d389a785aa016
Joonas Sarajärvi <joonas.sarajarvi@solita.fi>
2022-11-15 08:22:15 +0200

    Reduce default font size

--- init.el
+++ init.el
@@ -124,7 +124,7 @@
  ((eq window-system 'ns)
   ;; Font selection
   (if (member "Menlo" (font-family-list))
-      (set-face-attribute 'default nil :font "Menlo-11"))
+      (set-face-attribute 'default nil :font "Menlo-10"))
 
   ;; Avoid toolbar, but menubar is ok since the space is always used
   ;; in OS X.
