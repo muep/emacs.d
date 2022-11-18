8a66574b3bb5a7c6fad92747a4453f3687768ba0
Joonas Sarajärvi <muep@iki.fi>
2022-11-18 21:44:38 +0200

    Add keyboard shortcuts for org-roam

--- init.el
+++ init.el
@@ -51,6 +51,16 @@
   (string-replace "_" "-" txt))
 
 (when (functionp 'org-roam-version)
+  ;; Keybinding suggestions from
+  ;; https://lucidmanager.org/productivity/taking-notes-with-emacs-org-mode-and-org-roam/
+  (global-set-key (kbd "C-c n f") 'org-roam-node-find)
+  (global-set-key (kbd "C-c n r") 'org-roam-node-random)
+  (define-key org-mode-map (kbd "C-c n i") 'org-roam-node-insert)
+  (define-key org-mode-map (kbd "C-c n o") 'org-roam-id-get-create)
+  (define-key org-mode-map (kbd "C-c n t") 'org-roam-tag-add)
+  (define-key org-mode-map (kbd "C-c n a") 'org-roam-alias-add)
+  (define-key org-mode-map (kbd "C-c n l") 'org-roam-buffer-toggle)
+
   (advice-add 'org-roam-node-slug :filter-return #'underscore-to-dash)
 
   (setq org-roam-capture-templates
