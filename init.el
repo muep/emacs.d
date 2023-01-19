36719cc1f712698901adc121552cb573c32365ed
Joonas Sarajärvi <muep@iki.fi>
2023-01-19 20:54:53 +0200

    Add key binding for org-roam-dailies

--- init.el
+++ init.el
@@ -58,6 +58,7 @@
   ;; https://lucidmanager.org/productivity/taking-notes-with-emacs-org-mode-and-org-roam/
   (global-set-key (kbd "C-c n f") 'org-roam-node-find)
   (global-set-key (kbd "C-c n r") 'org-roam-node-random)
+  (global-set-key (kbd "C-c n d") 'org-roam-dailies-map)
   (define-key org-mode-map (kbd "C-c n i") 'org-roam-node-insert)
   (define-key org-mode-map (kbd "C-c n o") 'org-id-get-create)
   (define-key org-mode-map (kbd "C-c n t") 'org-roam-tag-add)
