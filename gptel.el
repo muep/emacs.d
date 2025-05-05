62de0edb5ceb2bf809c0fc124a1cdca355ebffec
Joonas Sarajärvi <muep@iki.fi>
2025-05-05 22:54:20 +0300

    Use Mistral for gptel backend

--- gptel.el
+++ gptel.el
@@ -1,5 +1,11 @@
 (use-package gptel
   :defer t
   :ensure t
+  :config
+  (setq gptel-backend
+        (gptel-make-openai "Mistral"
+          :key 'gptel-api-key
+          :host "api.mistral.ai"
+          :models '("mistral-large-latest" "mistral-tiny" "mistral-small" "mistral-medium")))
   :bind
   (("C-c å g s" . gptel-send)))
